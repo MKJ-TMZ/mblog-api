220a866f01ea7f75a93107a4a97ff4fe38bfe107
MTCode <1318844659@qq.com>
2022-03-16 21:26:55 +0800

    统一返回结果实体类

---
+++ src/main/java/com/mtcode/mblogapi/vo/Result.java
@@ -0,0 +1,52 @@
+package com.mtcode.mblogapi.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author TangMingZhang
+ * @date 2022/3/16
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Result {
+
+    private Integer code;
+    private String msg;
+    private Object data;
+
+    private Result(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public static Result ok(String msg, Object data) {
+        return new Result(200, msg, data);
+    }
+
+    public static Result ok(String msg) {
+        return new Result(200, msg);
+    }
+
+    public static Result ok() {
+        return new Result(200, "请求成功");
+    }
+
+    public static Result error(String msg) {
+        return new Result(500, msg);
+    }
+
+    public static Result error() {
+        return new Result(500, "异常错误");
+    }
+
+    public static Result create(Integer code, String msg, Object data) {
+        return new Result(code, msg, data);
+    }
+
+    public static Result create(Integer code, String msg) {
+        return new Result(code, msg);
+    }
+}
