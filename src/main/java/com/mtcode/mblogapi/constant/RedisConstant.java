2cc1423fdebb3da63c96926eaf966944a97463a5
MTCode <1318844659@qq.com>
2022-03-26 18:42:59 +0800

    redis以及mybatis plus配置

---
+++ src/main/java/com/mtcode/mblogapi/constant/RedisConstant.java
@@ -0,0 +1,11 @@
+package com.mtcode.mblogapi.constant;
+
+/**
+ * @author TangMingZhang
+ * @date 2022/3/26
+ */
+public class RedisConstant {
+    public static final String TAG = "tag:";
+
+    public static final String CATEGORY = "category:";
+}
