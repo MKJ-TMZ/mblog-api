3c5d14f7840b954479c36035e7ce145ee1489386
MTCode <1318844659@qq.com>
2022-03-26 18:42:25 +0800

    添加通用方法

--- src/main/java/com/mtcode/mblogapi/util/Func.java
+++ src/main/java/com/mtcode/mblogapi/util/Func.java
@@ -19,4 +19,14 @@ public class Func {
         }
         return false;
     }
+
+    /**
+     * 判断字符串是否非空
+     *
+     * @param str 待校验字符串
+     * @return 校验结果
+     */
+    public static boolean isNotEmptyAsString(String... str) {
+        return !isEmptyAsString(str);
+    }
 }
