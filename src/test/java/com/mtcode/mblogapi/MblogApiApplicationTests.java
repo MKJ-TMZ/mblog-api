3b4d347d0e11750a32be26cef84c7fa633854c80
MTCode <1318844659@qq.com>
2022-03-29 21:43:32 +0800

    生成账号测试类

--- src/test/java/com/mtcode/mblogapi/MblogApiApplicationTests.java
+++ src/test/java/com/mtcode/mblogapi/MblogApiApplicationTests.java
@@ -1,13 +1,45 @@
 package com.mtcode.mblogapi;
 
+import com.mtcode.mblogapi.entity.User;
+import com.mtcode.mblogapi.mapper.UserMapper;
+import com.mtcode.mblogapi.util.HashUtils;
+import com.mtcode.mblogapi.util.SpringContextUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @SpringBootTest
 class MblogApiApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void createUser() throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        UserMapper userMapper = SpringContextUtil.getBean(UserMapper.class);
+        User user = new User();
+        user.setUsername("admin");
+        user.setPassword(HashUtils.getBC(getMd5("admin")));
+        user.setRole("ROLE_admin");
+        userMapper.insert(user);
+    }
+
+    public static String getMd5(String text) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        byte[] bytes = md5.digest(text.getBytes(StandardCharsets.UTF_8));
+
+        StringBuilder builder = new StringBuilder();
+
+        for (byte aByte : bytes) {
+            builder.append(Integer.toHexString((0x000000FF & aByte) | 0xFFFFFF00).substring(6));
+        }
+
+        return builder.toString();
     }
 
 }
