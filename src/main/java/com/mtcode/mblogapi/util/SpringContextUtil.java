522632a4f71dc745fff01967de3f5577cc8a292d
MTCode <1318844659@qq.com>
2022-03-29 20:31:34 +0800

    Spring获取Bean工具类

---
+++ src/main/java/com/mtcode/mblogapi/util/SpringContextUtil.java
@@ -0,0 +1,64 @@
+package com.mtcode.mblogapi.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author TangMingZhang
+ * @date 2022/3/29
+ */
+@Component
+public class SpringContextUtil implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringContextUtil.applicationContext = applicationContext;
+    }
+
+    /**
+     * 获取applicationContext
+     *
+     * @return applicationContext
+     */
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    /**
+     * 通过name获取 Bean
+     *
+     * @param name name
+     * @return Bean
+     */
+    public static Object getBean(String name) {
+        return getApplicationContext().getBean(name);
+    }
+
+    /**
+     * 通过class获取Bean
+     *
+     * @param clazz class
+     * @param <T> T
+     * @return Bean
+     */
+    public static <T> T getBean(Class<T> clazz) {
+        return getApplicationContext().getBean(clazz);
+    }
+
+    /**
+     * 通过name,以及Class返回指定的Bean
+     *
+     * @param name name
+     * @param clazz class
+     * @param <T> T
+     * @return Bean
+     */
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return getApplicationContext().getBean(name, clazz);
+    }
+
+}
